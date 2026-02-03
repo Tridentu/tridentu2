a870f88fa0be27ed0cb78107c8dec14d23a0768e
Aerodos12 <weldon.henson97@gmail.com>
2026-02-03 11:31:10 -0500

    Tomohawk updated

--- skel/.config/tomohawk/tomohawk.tsx
+++ skel/.config/tomohawk/tomohawk.tsx
@@ -8,6 +8,8 @@ import { Astal } from "ags/gtk4"
 import { createPoll } from "ags/time"
 import { createBinding, createState } from "ags"
 import { For, onCleanup } from "ags"
+import { execAsync } from "ags/process"
+
 import Battery from "gi://AstalBattery"
 import Network from "gi://AstalNetwork"
 import PowerProfiles from "gi://AstalPowerProfiles"
@@ -116,89 +118,9 @@ interface NotificationProps {
     notification: Notifd.Notification
 }
 
-function Notification({ notification: n }: NotificationPros) {
-    return (
-	<Adw.Clamp maximumSize={400}>
-	   <box 
-	       widthRequest={400}
-	       class={`Notification ${urgency(n)}`}
-	       orientation={Gtk.Orientation.VERTICAL}
-	   >
-		<box class="header">
-          {(n.appIcon || isIcon(n.desktopEntry)) && (
-            <image
-              class="app-icon"
-              visible={Boolean(n.appIcon || n.desktopEntry)}
-              iconName={n.appIcon || n.desktopEntry}
-            />
-          )}
-          <label
-            class="app-name"
-            halign={Gtk.Align.START}
-            ellipsize={Pango.EllipsizeMode.END}
-            label={n.appName || "Unknown"}
-          />
-          <label
-            class="time"
-            hexpand
-            halign={Gtk.Align.END}
-            label={time(n.time)}
-          />
-          <button onClicked={() => n.dismiss()}>
-            <image iconName="window-close-symbolic" />
-          </button>
-        </box>
-        <Gtk.Separator visible />
-        <box class="content">
-          {n.image && fileExists(n.image) && (
-            <image valign={Gtk.Align.START} class="image" file={n.image} />
-          )}
-          {n.image && isIcon(n.image) && (
-            <box valign={Gtk.Align.START} class="icon-image">
-              <image
-                iconName={n.image}
-                halign={Gtk.Align.CENTER}
-                valign={Gtk.Align.CENTER}
-              />
-            </box>
-          )}
-          <box orientation={Gtk.Orientation.VERTICAL}>
-            <label
-              class="summary"
-              halign={Gtk.Align.START}
-              xalign={0}
-              label={n.summary}
-              ellipsize={Pango.EllipsizeMode.END}
-            />
-            {n.body && (
-              <label
-                class="body"
-                wrap
-                useMarkup
-                halign={Gtk.Align.START}
-                xalign={0}
-                justify={Gtk.Justification.FILL}
-                label={n.body}
-              />
-            )}
-          </box>
-        </box>
-        {n.actions.length > 0 && (
-          <box class="actions">
-            {n.actions.map(({ label, id }) => (
-              <button hexpand onClicked={() => n.invoke(id)}>
-                <label label={label} halign={Gtk.Align.CENTER} hexpand />
-              </button>
-            ))}
-          </box>
-        )}
-	   </box>
-	</Adw.Clamp>
-    );
-}
 
 
-
+       
 function THMain(){
    const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
    return (
@@ -211,6 +133,8 @@ function THMain(){
                <THClock />
                <THNetwork />
                <THBQS />
+               <button icon-name="system-shutdown" class="tomohawkLogout" onClicked={() => execAsync(["wlogout"])}></button>
+               
             </box>
            </centerbox>
         </window>

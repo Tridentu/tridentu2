496abd44fbc543d4ba332a66045e432caab09c2e
Aerodos12 <weldon.henson97@gmail.com>
2026-04-10 11:32:41 -0400

    NF_TABLES upgrade to .config

---
+++ profile.d/qt6.sh
@@ -0,0 +1,10 @@
+# Begin /etc/profile.d/qt6.sh
+
+QT6DIR=/opt/qt6
+
+pathappend $QT6DIR/bin           PATH
+pathappend $QT6DIR/lib/pkgconfig PKG_CONFIG_PATH
+
+export QT6DIR
+
+# End /etc/profile.d/qt6.sh
